0abd4d073f9ab5b40e7499bdbc8ea0e3a6838422
willyoliv <willyoliversilva@gmail.com>
2020-06-15 17:33:24 -0300

    Deploy da aplicação

--- api/settings.py
+++ api/settings.py
@@ -17,6 +17,16 @@ import spacy
 django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #Carregando o modelo
 NLP = spacy.load('api/Modelo')
 # Quick-start development settings - unsuitable for production
